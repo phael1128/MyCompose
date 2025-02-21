e0ca5d43a30eb861ac49f444015f22bcf10ce2c8
phael <phael.dev@kidsnote.com>
2025-02-21 23:17:58 +0900

    Compose UI Component 1 : Text
    squash
    
    sqush

--- 01Component/app/src/main/java/com/example/component/MainActivity.kt
+++ 01Component/app/src/main/java/com/example/component/MainActivity.kt
@@ -16,32 +16,5 @@ import com.example.component.ui.theme.ComponentTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ComponentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComponentTheme {
-        Greeting("Android")
     }
 }
