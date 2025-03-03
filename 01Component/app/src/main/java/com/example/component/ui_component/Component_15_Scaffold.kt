328e65e5ab419661dcf9d1edbed73de6cf011c0d
phael <phael.dev@kidsnote.com>
2025-03-03 20:59:12 +0900

    Compose UI Component 15 : Scaffold

---
+++ 01Component/app/src/main/java/com/example/component/ui_component/Component_15_Scaffold.kt
@@ -0,0 +1,81 @@
+package com.example.component.ui_component
+
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ComposeScaffold() {
+    // Scaffold를 사용하는 이유
+    // 앱의 주요 UI 요소(예: 상단 바, 하단 바, FAB 등)를 쉽게 배치할 수 있음.
+    // 레이아웃이 자동으로 정리되므로, 직접 배치할 때보다 더 깔끔하고 유지보수하기 쉬움.
+
+    val checked = remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Scaffold")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Image(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "뒤로가기"
+                        )
+                    }
+                },
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+
+                }
+            ) {
+
+            }
+        }
+    ) { paddingValue ->
+        Row (
+            modifier = Modifier.padding(top = paddingValue.calculateTopPadding())
+        ) {
+            CheckboxWithSlot(
+                checked = checked.value,
+                onCheckedChanged = {
+                    checked.value = !checked.value
+                }
+            ) {
+                Text(text = "컴포즈")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ComposeScaffoldPreview() {
+    ComposeScaffold()
+}
