cd82e0e0147ee170122aee420c596fb2500aee43
phael <phael.dev@kidsnote.com>
2025-04-30 00:21:59 +0900

    hiltViewModel 사용을 위한 dependency 추가

--- ComposeStudy/app/build.gradle.kts
+++ ComposeStudy/app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     // hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
 
     // gson
     implementation(libs.gson)
