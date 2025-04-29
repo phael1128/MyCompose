510fdfe10b98d419058d26e80a1644d4c140c0ea
phael <phael.dev@kidsnote.com>
2025-04-29 22:58:09 +0900

    hilt dependency 추가

--- ComposeStudy/app/build.gradle.kts
+++ ComposeStudy/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
 }
 
@@ -72,4 +73,8 @@ dependencies {
     //LiveData & Flow
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.compose.runtime:runtime-livedata:1.7.8")
+
+    // hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
